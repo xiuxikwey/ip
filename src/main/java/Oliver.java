1c13f953ed47cfb578c4be81106f0e8709b8713a
xiuxi <177907531+xiuxikwey@users.noreply.github.com>
2026-01-20 17:44:41 +0800

    Add list functionality
    Keeps track of user input and repeats past input by typing list.

--- src/main/java/Oliver.java
+++ src/main/java/Oliver.java
@@ -1,28 +1,45 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Oliver {
+    private static ArrayList<String> strArr = new ArrayList<String>();
 
-    public static void event_handler() {
+    private static void event_handler() {
         try(Scanner sc = new Scanner(System.in)) {
             while (sc.hasNextLine()) {
                 String str = sc.nextLine();
-                if (str.equals("bye")) {
+                if (str.equalsIgnoreCase("bye")) {
                     sayGoodbye();
+                } else if (str.equalsIgnoreCase("list")) {
+                    readItems();
                 } else {
-                    speak(str);
+                    storeItem(str);
                 }
             }
         }
-        
     }
 
-    public static void speak(String input) {
+    private static void storeItem(String input) {
+        strArr.add(input);
+        speak("Stored \"" + input + "\".");
+    }
+
+    private static void readItems() {
+        ListIterator<String> iter = strArr.listIterator();
+        speak("Reading from my records:");
+        while (iter.hasNext()) {
+            speak(iter.nextIndex() + ": " + iter.next());
+        }
+    }
+
+    private static void speak(String input) {
         System.out.println("\u001B[31m"
             + input
             + "\u001B[0m");
     }
 
-    public static void greet() {
+    private static void greet() {
         String drawing = """
          ___               ___
         (___\\  /~\\   /~\\  /___)
@@ -32,10 +49,15 @@ public class Oliver {
         *****************************
         """;
         System.out.println(drawing);
-        speak("Oliver, King Of The Night, at your service!");
+        speak(
+        """
+        Oliver, King Of The Night, at your service!
+        I keep track of things you say, which you can recall by saying \"list\".
+        You can exit by saying \"bye\"!
+        """);
     }
 
-    public static void sayGoodbye() {
+    private static void sayGoodbye() {
         String drawing = """
               A    A
             <  U w U >
